90e79131dfbbedf8b2d0b4067491dc6abde114c0
mgbec <70477486+mgbec@users.noreply.github.com>
2025-10-28 13:23:12 -0500

    Enhance __init__.py with additional configurations

--- src/sbom_consolidator/config/__init__.py
+++ src/sbom_consolidator/config/__init__.py
@@ -1,9 +1,24 @@
-"""
+ """
 Configuration management for the SBOM consolidator system.
 """
 
-from .config_manager import ConfigManager
+from .config_manager import (
+    ConfigManager, AppConfig, GitHubConfig, OutputConfig, ScanningConfig,
+    LoggingConfig, VulnerabilityConfig, ConsolidationConfig, AWSConfig,
+    BedrockConfig, get_config_manager, get_config
+)
 
 __all__ = [
-    "ConfigManager"
+    "ConfigManager",
+    "AppConfig",
+    "GitHubConfig",
+    "OutputConfig",
+    "ScanningConfig",
+    "LoggingConfig",
+    "VulnerabilityConfig",
+    "ConsolidationConfig",
+    "AWSConfig",
+    "BedrockConfig",
+    "get_config_manager",
+    "get_config"
 ]
